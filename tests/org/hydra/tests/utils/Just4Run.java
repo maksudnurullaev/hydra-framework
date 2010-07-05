85a0e101dfc3454d623bbe7300a4110cdd309857
maksud.nurullaev <maksud.nurullaev@3d67d3f9-dc53-623c-90de-2702e2060132>
2010-07-05 13:09:31 +0000

    Job -save-> Home

--- tests/org/hydra/tests/utils/Just4Run.java
+++ tests/org/hydra/tests/utils/Just4Run.java
@@ -1,12 +1,9 @@
 package org.hydra.tests.utils;
 
-import java.util.Map;
-
 import org.hydra.db.server.CassandraAccessorBean;
 import org.hydra.db.server.CassandraDescriptorBean;
-import org.hydra.db.server.CassandraVirtualPath;
+import org.hydra.tests.db.fmd.TestFMDKspCfIdLinks;
 import org.hydra.utils.DBUtils;
-import org.hydra.utils.Result;
 
 public class Just4Run {
 	static String userID = "user0";
@@ -14,15 +11,16 @@ public class Just4Run {
 	static CassandraDescriptorBean descriptor = DBUtils.getDescriptor();
 	
 	public static void main(String[] args) {
-		initTestData();
+		TestFMDKspCfIdLinks.initTestDataStage1User();
+		TestFMDKspCfIdLinks.initTestDataStage2Articles();
 		
-		CassandraVirtualPath path2Delete = new CassandraVirtualPath(descriptor,
-				Utils4Tests.KSMAINTEST_Users + "." + userID);		
-		
-		System.out.println("Delete2path: " + path2Delete.getPath());
-		
-		accessor.delete(path2Delete);
-		// Mutation2Delete.generate(path2Delete);
+//		CassandraVirtualPath path2Delete = new CassandraVirtualPath(descriptor,
+//				Utils4Tests.KSMAINTEST_Users + "." + userID);		
+//		
+//		System.out.println("Delete2path: " + path2Delete.getPath());
+//		
+//		accessor.delete(path2Delete);
+//		// Mutation2Delete.generate(path2Delete);
 		
 		// ############# delete cascade
 //		CassandraVirtualPath path = new CassandraVirtualPath(descriptor, 
@@ -37,37 +35,6 @@ public class Just4Run {
 //		
 //		result = Utils4Tests.getAccessor().delete(path);		
 //		Assert.assertTrue(result.isOk());
-			
-	}
-
-	public static void initTestData() {
-
-		// ############### init test data
-		// init USER
-		Map<String, Map<String, String>> user = Utils4Tests.initTestUsers(1);
-		userID = (String) user.keySet().toArray()[0];
-		CassandraVirtualPath path2Users = new CassandraVirtualPath(descriptor,
-				Utils4Tests.KSMAINTEST_Users);
-		Result result = accessor.update(path2Users, DBUtils.convert2Bytes(user));
-		
-		if(result.isOk())
-			System.out.println("Initial user data merged!");
-		else
-			System.out.println(result.getResult());
-		
-		Map<String, Map<String, String>> articles = 
-			Utils4Tests.initTestArticles(15);		
-		// init linked ARTICLES
-		CassandraVirtualPath path2UsersIDArticles = new CassandraVirtualPath(
-				descriptor,
-				String.format(Utils4Tests.KSMAINTEST_Users_S_Articles, userID));
-		result = accessor.update(path2UsersIDArticles, DBUtils
-				.convert2Bytes(articles));
-		
-		if(result.isOk())
-			System.out.println("Initial linked articles data merged!");
-		else
-			System.out.println(result.getResult());
 	}
 
 }
