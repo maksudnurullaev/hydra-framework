c7a274846ad4731e6b08ec67ae90f0ed38d69cdf
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@3d67d3f9-dc53-623c-90de-2702e2060132>
2011-08-03 12:42:10 +0000

    Add minidetector middleware

--- ersms.ru/index/urls.py
+++ ersms.ru/index/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls.defaults import *
-import views 
+#import views 
+
+#urlpatterns = patterns('',
+#   (r'^$', views.index),
+#    (r'^$', views.index),
+#)
+
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    (r'^$', views.index),
+    (r'^$', direct_to_template, {'template': 'index/index.html'}),
 )
+
