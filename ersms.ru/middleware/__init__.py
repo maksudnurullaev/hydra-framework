7191a26ca29ee0376b99dc4175622f4dc427658e
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@3d67d3f9-dc53-623c-90de-2702e2060132>
2011-08-11 05:39:18 +0000

    Strip output html comments

--- ersms.ru/middleware/__init__.py
+++ ersms.ru/middleware/__init__.py
@@ -4,13 +4,16 @@ from django.conf import settings
  
 RE_MULTISPACE = re.compile(r"\s{2,}")
 RE_NEWLINE = re.compile(r"\n")
+RE_HTMLCOMMENTS = re.compile('\<![ \r\n\t]*(--([^\-]|[\r\n]|-[^\-])*--[ \r\n\t]*)\>')
  
 class MinifyHTMLMiddleware(object):
     @staticmethod
     def process_response(request, response):
-        print "Hello"
         if 'text/html' in response['Content-Type'] and settings.COMPRESS_HTML:
             response.content = strip_spaces_between_tags(response.content.strip())
             response.content = RE_MULTISPACE.sub(" ", response.content)
             response.content = RE_NEWLINE.sub("", response.content)
+            response.content = RE_HTMLCOMMENTS.sub("", response.content)
         return response
+
+
