7f6bd44699c29792bffee18ca3fe38b4a9510da2
maksud.nurullaev <maksud.nurullaev@3d67d3f9-dc53-623c-90de-2702e2060132>
2011-09-07 10:01:02 +0000

    Delete baseBox impelemntation and add waiting functionality for  zfileuz

--- trunk/WebContext/files/ZFileUz/js/main.js
+++ trunk/WebContext/files/ZFileUz/js/main.js
@@ -63,3 +63,32 @@ ZFileUz.try2SendFile = function(){
 		$(ZFileUz.Content).innerHTML = "Ждем ответа от сервера...";
 	} 
 };
+
+// SET GLOBAL HOOKS 
+ZFileUz.preHook = function(){
+	if($('waiting')){
+		$('waiting').fade('show');
+	}
+	if($('topmenu')){
+		$('topmenu').fade('hide');
+	}
+	if($('contentcontainer')){
+		$('contentcontainer').fade('hide');
+	}
+	
+};
+ZFileUz.postHook = function(){
+	if($('waiting')){
+		$('waiting').fade('hide');
+	}
+	if($('topmenu')){
+		$('topmenu').fade('show');
+	}
+	if($('contentcontainer')){
+		$('contentcontainer').fade('show');
+	}
+};
+if(dwr.engine){
+	dwr.engine.setPreHook(ZFileUz.preHook);
+	dwr.engine.setPostHook(ZFileUz.postHook);
+};
