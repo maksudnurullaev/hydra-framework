091c1405fb43bc5c768fcf1a1c4af974fb861306
maksud.nurullaev <maksud.nurullaev@3d67d3f9-dc53-623c-90de-2702e2060132>
2012-01-25 12:41:55 +0000

    Update spring libraries

--- trunk/src/org/hydra/messages/interfaces/IMessage.java
+++ trunk/src/org/hydra/messages/interfaces/IMessage.java
@@ -1,11 +1,12 @@
 package org.hydra.messages.interfaces;
 
+import java.io.Serializable;
 import java.util.Map;
 
 import org.directwebremoting.io.FileTransfer;
 import org.hydra.utils.Result;
 
-public interface IMessage {	
+public interface IMessage extends Serializable {	
 	Map<String, String> getData();
 	String getError();
 	Result set2HttpSession(String inKey, Object inObj);
