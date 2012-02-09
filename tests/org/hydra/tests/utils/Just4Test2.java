590b323a8f1c93a5f6e8518dd67c2b6d005b15b7
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@3d67d3f9-dc53-623c-90de-2702e2060132>
2012-02-09 17:15:46 +0000

    Update from home

--- tests/org/hydra/tests/utils/Just4Test2.java
+++ tests/org/hydra/tests/utils/Just4Test2.java
@@ -1,15 +1,12 @@
 package org.hydra.tests.utils;
-import java.net.*;
 
 public class Just4Test2 {
 
-	 public static void main(String [] args) {
-	 try {
-		  InetAddress addr = InetAddress.getLocalHost();
-		  String hostname = addr.getHostName();
-		  System.out.println("hostname="+hostname);
-		  } catch (UnknownHostException e) {
-			  System.out.println(e.getMessage());
-		  }
-	  }
+	public static void main(String[] argv){
+		String str = "</textarea> --- </texTArea>";
+
+		String result = str.replaceAll("(?i)</TextArea>", "[[Dictionary|Template|template.textarea.endtag|html]]");
+		System.out.println("After replacement:\n" + "   " + result);
+
+	}
 }
