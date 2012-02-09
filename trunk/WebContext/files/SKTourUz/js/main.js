0558a4f144a931c59081e7138f8e6e8f8eb14a53
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@3d67d3f9-dc53-623c-90de-2702e2060132>
2012-02-09 13:24:21 +0000

    Move to server

--- trunk/WebContext/files/SKTourUz/js/main.js
+++ trunk/WebContext/files/SKTourUz/js/main.js
@@ -1,7 +1,32 @@
 /* Initial global setups */
-if (Test2Uz == null) {
-    var Test2Uz = { 
+if (SKTourUz == null) {
+    var SKTourUz = { 
     	'version': '0.0.1a',
-    	'topNav': 'topNav'
+    	'topNav': 'topNav',
+	'MainContent': 'main.content'
     	};
+};
+
+SKTourUz.setMainContent = function(inEl){    
+    if(Globals.pageBusy){
+        return;
+    }
+    $$('#topmenuul a').each(function(el){
+         if(el.id == inEl.id){
+            el.getParent().setProperty('class', 'active');
+        }else{
+            el.getParent().setProperty('class', '');
+        }
+    });  
+    
+    var content = '[[';
+    content += ('DB|Template|Main.' + inEl.id + '.Page|span');
+    content += ']]';
+
+    Globals.sendMessage({
+        handler: 'General'
+        , action:  'getContent'
+        , content: content
+        , dest: SKTourUz.MainContent
+    });        
 };
