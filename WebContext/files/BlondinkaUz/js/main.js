cd60236c1c59e052d1f9f2642a3c2a4be9d11a4e
maksud.nurullaev <maksud.nurullaev@3d67d3f9-dc53-623c-90de-2702e2060132>
2012-03-20 10:25:36 +0000

    Fixed js for site

--- WebContext/files/BlondinkaUz/js/main.js
+++ WebContext/files/BlondinkaUz/js/main.js
@@ -1,24 +1,15 @@
 /* Initial global setups */
-if (SKTourUz == null) {
-    var SKTourUz = { 
-        'version': '0.0.1a',
-        'topNav': 'topNav',
-        'MainContent': 'main_content'
+if (BlondinkaUz == null) {
+    var BlondinkaUz = { 
+        'version': '0.0.1a' ,
+		'MainContent': 'main_content'
         };
 };
 
-SKTourUz.setMainContent = function(aEl){    
+BlondinkaUz.setMainContent = function(aEl){    
     if(Globals.pageBusy){
         return;
     }
-
-    jQuery('#topmenuul a').each(function(index, el){
-        if(el.id == aEl.id){
-            jQuery(el.parentNode).addClass('active');
-        }else{
-            jQuery(el.parentNode).removeClass('active');
-        }
-    });  
     
     var content = '[[';
     content += ('DB|Template|Main.' + aEl.id + '.Page|span');
@@ -28,23 +19,6 @@ SKTourUz.setMainContent = function(aEl){
         handler: 'General'
         , action:  'getContent'
         , content: content
-        , dest: SKTourUz.MainContent
-    });
-};
-
-SKTourUz.setContent = function(aEl, contentHolderId){    
-    if(Globals.pageBusy){
-        return;
-    }
-
-    var content = '[[';
-    content += ('DB|Template|Content.' + aEl.id + '.Page|span');
-    content += ']]';
-
-    Globals.sendMessage({
-        handler: 'General'
-        , action:  'getContent'
-        , content: content
-        , dest: contentHolderId
+        , dest: BlondinkaUz.MainContent
     });
 };
